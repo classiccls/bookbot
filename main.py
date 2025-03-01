db604698a78ae6d05556601b4e187974673fd9b6
classiccls <classiccls@gmail.com>
2025-03-02 00:26:37 +0100

    05.Finalowa wersja projektu

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from stats import count_words, count_characters, sorting_dictionary, get_book_text, print_info
+import sys
 
-path_to_frankenstein = "books/frankenstein.txt"
-path_to_shit = "books/shit.txt"
+#path_to_frankenstein = "books/frankenstein.txt"
+#path_to_shit = "books/shit.txt"
 
 def main():
-    file_contents = get_book_text(path_to_frankenstein)
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_contents = get_book_text(sys.argv[1])
     number_of_words = count_words(file_contents)
-    #print(f"{count_words(file_contents)} words found in the document")
     new_dictionary = count_characters(file_contents)
     new_dictionary = sorting_dictionary(new_dictionary)
-    #print(new_dictionary)
-    print_info(path_to_frankenstein, number_of_words, new_dictionary)
+    print_info(sys.argv[1], number_of_words, new_dictionary)
 
 main()
